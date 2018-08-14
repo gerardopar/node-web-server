c06ab612978d4e0162b1b6165e5448ba47075c59
Gerardo Paredes <gerardoparedes@Gerardos-MacBook-Pro.local>
2018-08-13 23:06:06 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,74 @@
+/* - - [ IMPORTING MODULES ] - - */
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+/* - - [ SERVER CODE / APP CODE ] - - */
+let app = express(); //initializing express server
+
+hbs.registerPartials(__dirname + '/views/partials/'); //registering template partials
+app.set('view engine', 'hbs'); //setting up our templating engine
+
+//creating a server log system for users 
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+//creating a maintenance page
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); //setting up public route
+
+hbs.registerHelper('getCurrentYear', () => { //registering a reusable function
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => { //registering a reusable function
+    return text.toUpperCase();
+});
+
+//root/home route
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        greeting: 'Hello Gerardo!'
+    });
+
+});
+
+//about route
+app.get('/about', (req, res) => {
+    
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+
+});
+
+//404/bad route
+app.get('/bad', (req, res) => {
+    
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+
+});
+
+//server up on port 3000
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
+
+//this command keeps an eye on the js & hbs files
+//nodemon server.js -e js,hbs
